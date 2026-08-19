8e1242872ee53371730eeb9821c8ee77025306b9
Nokha Magomadov <nmagomad@k0r2p3.42mulhouse.fr>
2026-08-19 13:19:13 +0200

    edit: conform to prettier

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,22 +1,35 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      c = { "clang_format" },
-      cpp = { "clang_format" },
-      ["python"] = { "black", "ruff" },
-      -- python = { "ruff" },
-    },
-    formatters = {
-      clang_format = {
-        command = "clang-format",
-        -- используем assume-filename чтобы clang-format подбирал стиль по имени файла
-        args = { "--assume-filename", "$FILENAME" },
-        -- (опционально) если хочешь, можно добавить range_args для conform,
-        -- но ниже я дам более надёжный keymap для выделения.
-      },
-    },
-    -- можно также включить автоматическое форматирование при сохранении:
-    -- format_on_save = true,
-  },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			c = { "clang_format" },
+			cpp = { "clang_format" },
+			["python"] = { "black", "ruff" },
+			-- python = { "ruff" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			json = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+		},
+		formatters = {
+			clang_format = {
+				command = "clang-format",
+				-- используем assume-filename чтобы clang-format подбирал стиль по имени файла
+				args = { "--assume-filename", "$FILENAME" },
+				-- (опционально) если хочешь, можно добавить range_args для conform,
+				-- но ниже я дам более надёжный keymap для выделения.
+			},
+			-- prettier = {
+			-- 	command = "prettier",
+			-- 	-- Автоматически найдет локальный prettier,
+			-- 	-- если он есть в node_modules/.bin
+			-- 	cwd = function()
+			-- 		return vim.fn.getcwd()
+			-- 	end,
+			-- },
+		},
+		-- можно также включить автоматическое форматирование при сохранении:
+		-- format_on_save = true,
+	},
 }
